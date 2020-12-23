577c1996bafed9b71bce3e6874aa8fd5c0c8cb21
Shaun <shauncuier@gmail.com>
2020-12-23 22:46:00 +0600

    update new file

---
+++ Basic Layout/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <h1>This Is Footer</h1>
+</footer>
+</body>
+</html>
---
+++ MySQL/index.php
@@ -0,0 +1,34 @@
+<?php
+    include_once 'includes/db.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Database Practice</title>
+</head>
+<body>
+    <?php
+        $sql="SELECT * FROM users;";
+        $result = mysqli_query($conn,$sql);
+        $resultCheck = mysqli_num_rows($result);
+
+        if ($resultCheck > 0) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo $row["user_first"]. "<br><br>";
+                echo $row["user_last"]. "<br><br>";
+                echo $row["user_email"]. "<br><br>";
+            }
+        }
+    ?>
+
+
+    <br><br><br><br>
+
+    <?php
+    $sql="SELECT * FROM users;";
+    mysqli_query($conn,$sql);
+    ?>
+</body>
+</html>
