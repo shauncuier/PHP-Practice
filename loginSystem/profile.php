867ed19533b41cdd49421c2a1c2bb8002e6bc324
Jashedul Islam Shaun <71716179+shauncuier@users.noreply.github.com>
2022-08-10 12:44:12 +0600

    some file deleted

--- loginSystem/profile.php
+++
@@ -1,6 +0,0 @@
-<?php include_once 'header.php' ?>
-            <div class="jumbotron text-center">
-                <h1>My Profile</h1>
-                <p>Successfully LogIn</p>
-            </div>
-<?php include_once 'footer.php' ?>
